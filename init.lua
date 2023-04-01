4708b2394b042467e25d52e004ce908ea3878417
Wladimir <wl.urzua@duocuc.cl>
2023-04-01 00:24:38 -0300

    test

--- init.lua
+++ init.lua
@@ -2,9 +2,8 @@ local M = {}
 
 M.xdeps = require("xdeps")
 
-tools = M.xdeps.tools
-check_tools = M.xdeps.check_tools()
+--tools = M.xdeps.tools
+--check_tools = M.xdeps.check_tools()
 
-vim.keymap.set("n", "<leader><leader>cz", function() print("hello") end)
 
 return M
